bf171aa1ab884dd473d16b7aaa369ffbeb552f0b
Danil Ratnikov <feed111feed111@mail.ru>
2022-08-31 11:34:12 +0700

    v4.2

--- filters/all_filters.py
+++ filters/all_filters.py
@@ -3,15 +3,16 @@ from aiogram.dispatcher.filters import BoundFilter
 
 from data.config import admins
 
-# Проверка на написания сообщения в ЛС бота
+# Проверка на написания сообщения в ЛС бота)
 class IsPrivate(BoundFilter):
     async def check(self, message: types.Message):
         return types.ChatType.PRIVATE == message.chat.type
 
-# Проверка на админа
+# Проверка на админа)
 class IsAdmin(BoundFilter):
     async def check(self, message: types.Message):
         if message.from_user.id in admins:
             return True
         else:
             return False
+        
